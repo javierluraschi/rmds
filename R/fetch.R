23055f4f2ffa2a14a4a9bbea0208edd04f8007ff
Javier Luraschi <javierluraschi@hotmail.com>
2019-08-30 15:32:09 -0700

    add list of terms used to search github files

--- R/fetch.R
+++ R/fetch.R
@@ -1,10 +1,23 @@
-#' Feetch Rmds
+#' Serch Terms
+#'
+#' Relevant search terms to use when searching for Rmds.
+#'
+#' @export
+rmds_terms <- function() {
+  c("keras", "dplyr", "tidyverse", "readr", "caret",
+    "DBI", "odbc", "foreign", "tidyr", "lubridte",
+    "ggplot2", "car", "mgcv", "glmnet", "survival",
+    "shiny", "zoo", "data.table", "parallel", "jsonlite",
+    "httr")
+}
+
+#' Fetch Rmds
 #'
 #' Fetches Rmds from various public locations and stores results using the
 #' \code{pins} package under an \code{rmds} board.
 #'
 #' @export
-rmds_fetch <- function(terms = "sparklyr", board = "rmds") {
+rmds_fetch <- function(terms = rmds_terms(), board = "rmds") {
   if (!board %in% pins::board_list())
     pins::board_register("github", board, repo = "javierluraschi/rmds", branch = "datasets")
 
